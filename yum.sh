6e92904c2ee257128d16ac6c2555cf51865ab997
elvis.ngeh <evis.bantar@gmail.com>
2022-11-28 22:23:02 -0500

    more corrections

--- yum.sh
+++ yum.sh
@@ -3,12 +3,16 @@ echo "what pkg do you want to install ?"
 read pkg 
 sudo yum install $pkg -y >> /tmp/yum.log
 sudo cat /tmp/yum.log | grep "Nothing to do"
-message="Nothing to do"
 if [ $message -eq "Nothing to do" ] 
 then
+message="Nothing to do"
 echo "pkg is already installed "
-else 
+echo "no work to do ,get a beer"
+elif 
 echo "continue with the installation"
+then
+echo "enter your pkg to continue installation"
+read pkg
 fi
 sudo $pkg yum -y >> /tmp/yum.log
 cd /tmp
