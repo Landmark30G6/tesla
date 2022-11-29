257d8b62c10cf40712674d8b8f4c8f89c61ebf98
elvis.ngeh <evis.bantar@gmail.com>
2022-11-28 21:47:15 -0500

    another extra line

--- yum.sh
+++ yum.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "what do you want to install ?"
-read install 
-sudo yum $install -y >> /tmp/yum.log
+echo "what pkg do you want to install ?"
+read pkg 
+sudo yum install $pkg -y >> /tmp/yum.log
 sudo cat /tmp/yum.log | grep "Nothing to do"
 message="Nothing to do"
 if [ $message -eq "Nothing to do" ] 
 then
 echo "pkg is already installed "
 else "continue with the installation"
-sudo $install yum -y >> /tmp/yum.log
+sudo $pkg yum -y >> /tmp/yum.log
 cd /tmp
 sudo gzip yum.log
